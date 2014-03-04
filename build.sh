7917678fd27956a4beebbab6cfecbe8487839b67
torrented <wallace.ryan10@gmail.com>
2014-03-03 20:08:00 -0500

    Make tools universal and move them to LOCAL_BUILD_DIR

--- build.sh
+++ build.sh
@@ -100,9 +100,9 @@ mkdir -p $UPDATE_ROOT/system/lib/modules
 find . -name '*.ko' -exec cp {} $UPDATE_ROOT/system/lib/modules/ \;
 
 mkdir -p $UPDATE_ROOT/META-INF/com/google/android
-cp -r $TOOLS_DIR/update-binary-installer $UPDATE_ROOT/META-INF/com/google/android
+cp $TOOLS_DIR/update-binary-installer $UPDATE_ROOT/META-INF/com/google/android
 cp -r $TOOLS_DIR/aroma $UPDATE_ROOT/META-INF/com/google/android
-cp -r $TOOLS_DIR/aroma-config $UPDATE_ROOT/META-INF/com/google/android
+cp $TOOLS_DIR/aroma-config $UPDATE_ROOT/META-INF/com/google/android
 cp -r $TOOLS_DIR/tools $UPDATE_ROOT/
 cp $TOOLS_DIR/update-binary $UPDATE_ROOT/META-INF/com/google/android
 (
